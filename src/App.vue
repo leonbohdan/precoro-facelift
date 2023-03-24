c3d25503af6c5124fe74790a02e96acd595f00e9
bohdan leonets <bohdan@mytowerapp.com>
2023-03-24 13:08:20 +0200

    add default fonts and colors

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,23 @@
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">
+  <h1 class="text-3xl">
     Hello world!
+
+    <div class="font-sans font-semibold">
+      <span class="font-medium">
+        Inter
+      </span>
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias autem beatae consequatur dolorem dolores, eius eligendi esse explicabo minima molestiae nobis numquam perferendis placeat quo reprehenderit sed totam veritatis!
+    </div>
+
+    <div class="font-serif font-bold">
+      <span class="font-medium">
+        Literata
+      </span>
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi corporis exercitationem expedita mollitia non. Amet beatae, dolore dolorem doloremque facere nam neque pariatur provident rerum, sapiente sed sit soluta tempora.
+    </div>
   </h1>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,29 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Literata', 'serif'],
+    },
+
+    colors: {
+      current: 'currentColor',
+      'primary': 'var(--custom-blue-color)',
+      'primary2': 'var(--custom-blue2-color)',
+      'success': 'var(--custom-green-color)',
+      'c-black': 'var(--custom-black-color)',
+      'c-gray': 'var(--custom-gray-color)',
+      'bg-blue': 'var(--custom-blue-bg)',
+      'bgh-blue': 'var(--custom-blue-bg-hover)',
+      'bg-blue2': 'var(--custom-blue-bg2)',
+      'bgh-blue2': 'var(--custom-blue-bg2-hover)',
+      'bg-green': 'var(--custom-green-bg)',
+      'bgh-green': 'var(--custom-green-bg-hover)',
+    },
+
     extend: {},
   },
   plugins: [],
